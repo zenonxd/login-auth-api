3beb977c40675b45a900c364d756f193705f8c8b
zenonxd <olavomoreira99@gmail.com>
2024-11-25 15:53:16 -0300

    feat: creating methods repository

--- src/main/java/com/moreira/loginauthapi/domain/repositories/RoleRepository.java
+++ src/main/java/com/moreira/loginauthapi/domain/repositories/RoleRepository.java
@@ -3,8 +3,12 @@ package com.moreira.loginauthapi.domain.repositories;
 import com.moreira.loginauthapi.domain.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface RoleRepository extends JpaRepository<Role, String> {
     Role findRoleByName(String roleUser);
 
     Role findByName(String role);
+
+    Set<Role> findByNameIn(Set<String> names);
 }
