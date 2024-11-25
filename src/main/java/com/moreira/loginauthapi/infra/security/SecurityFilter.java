9505449205775002752885ef330f11ef49a2945e
zenonxd <olavomoreira99@gmail.com>
2024-11-24 23:44:47 -0300

    feat: creating and receiving list of roles

---
+++ src/main/java/com/moreira/loginauthapi/infra/security/SecurityFilter.java
@@ -0,0 +1,64 @@
+package com.moreira.loginauthapi.infra.security;
+
+import com.moreira.loginauthapi.domain.entities.User;
+import com.moreira.loginauthapi.domain.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;  // Servico para gerar e validar o JWT
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        var token = this.recoverToken(request);
+        var login = tokenService.validateToken(token);
+
+        if (login != null) {
+            // Aqui o ideal é buscar o usuário no banco de dados para adicionar suas roles
+            User user = userRepository.findByEmail(login)
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+
+            //Gerar as roles diretamente a partir das claims no token
+            List<SimpleGrantedAuthority> authorities = user.getRoles().stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .collect(Collectors.toList());
+
+
+            var authentication = new UsernamePasswordAuthenticationToken(login, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) return null;
+
+        return authHeader.replace("Bearer ", "");
+    }
+}
